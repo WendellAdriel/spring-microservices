46637275cf24c29088a012abebcaeaec868a9cfb
Ramazan Sakin <ramazansakin63@gmail.com>
2020-09-01 11:48:08 +0300

    Lombok integration & Refactoring on article-service

--- article-service/src/main/java/br/com/codeshare/microservices/articles/models/Article.java
+++ article-service/src/main/java/br/com/codeshare/microservices/articles/models/Article.java
@@ -1,7 +1,16 @@
 package br.com.codeshare.microservices.articles.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 import java.util.Date;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Article {
 
     private Integer id;
@@ -9,55 +18,4 @@ public class Article {
     private Date publishDate;
     private Integer authorId;
 
-    public Article() {
-    }
-
-    public Article(Integer id, String name, Date publishDate, Integer authorId) {
-        this.id = id;
-        this.name = name;
-        this.publishDate = publishDate;
-        this.authorId = authorId;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Date getPublishDate() {
-        return publishDate;
-    }
-
-    public void setPublishDate(Date publishDate) {
-        this.publishDate = publishDate;
-    }
-
-    public Integer getAuthorId() {
-        return authorId;
-    }
-
-    public void setAuthorId(Integer authorId) {
-        this.authorId = authorId;
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", publishDate=" + publishDate +
-                ", authorId=" + authorId +
-                '}';
-    }
 }
